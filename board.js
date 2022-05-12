d832f3bc1992e9674a96a31fc5fed27c51de35a1
Park-min-hyoung <qkralswgud1324@naver.com>
2022-05-12 11:39:18 +0900

    Feat: Add 게시글 localstorage에 저장
    게시글 localstorage에 객체가 포함되 있는 배열 형태로 저장

--- board.js
+++ board.js
@@ -3,20 +3,27 @@ const stickerTitle = document.querySelector("#board-form input[type=text]");
 const stickerContent = document.querySelector("#board-form textarea");
 const stickerBoard = document.querySelector(".board-container");
 
+const STIKER_KEY = "sticker";
+
+let stickers = [];
+
+let stickerLocalSave = () => {
+  localStorage.setItem(STIKER_KEY, JSON.stringify(stickers));
+};
+
 let removeSticker = (event) => {
   const rmSticker = event.target.parentElement.parentElement;
   rmSticker.remove();
 };
 
-let makeSticker = (event) => {
-  event.preventDefault();
+let makeSticker = (sticker) => {
   stickerDiv = document.createElement("div");
   stickerDiv.classList.add("board-container__sticker");
 
   titleDiv = document.createElement("div");
-  titleDiv.innerText = stickerTitle.value;
+  titleDiv.innerText = sticker.title;
   contentDiv = document.createElement("div");
-  contentDiv.innerText = stickerContent.value;
+  contentDiv.innerText = sticker.content;
   closeSpan = document.createElement("span");
   closeSpan.insertAdjacentHTML(
     "beforeend",
@@ -33,4 +40,17 @@ let makeSticker = (event) => {
   stickerContent.value = "";
 };
 
-stickerForm.addEventListener("submit", makeSticker);
+// by 민형, 사용자가 작성한 내용을 받아 처리_220512
+let receiveValue = (event) => {
+  event.preventDefault();
+  const stickerObj = {
+    title: stickerTitle.value,
+    content: stickerContent.value,
+  };
+
+  stickers.push(stickerObj);
+  stickerLocalSave(stickerObj);
+  makeSticker(stickerObj);
+};
+
+stickerForm.addEventListener("submit", receiveValue);
