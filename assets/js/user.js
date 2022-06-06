109f8a1f58d81c3b52b99ebccbd83266218803b0
Park-min-hyoung <qkralswgud1324@naver.com>
2022-06-06 16:46:21 +0900

    🚑 Fix: 수정 검색 결과가 남아있는 문제
    검색하고 로그아웃 한 후 다시 로그인 하면 검색 결과가 그대로 출력 되는 문제 해결
    로그아웃 하고 다시 로그인 했을 때 검색 결과가 아닌 업로드 한 모든 스티커가 있어야 하므로 수정

--- assets/js/user.js
+++ assets/js/user.js
@@ -19,7 +19,6 @@ const USER_KEY = "user";
 // by 민형, 로그인 유무에 따라 board 정보 랜더링_220515
 let boardDisplay = () => {
   stickerBoardDisplay.classList.toggle("hidden");
-  // by 민형, board.js의 searchForm 변수 사용_220526
   searchFormDisplay.classList.toggle("hidden");
 };
 
@@ -29,6 +28,14 @@ let logOut = ({ target }) => {
   userNameInput.value = "";
   userLoginDiv.classList.remove("hidden");
   stickerFormDisplay.classList.add("hidden");
+
+  // by 민형, board.js에서 변수 및 메소드 가져와서 사용, 나중에 export로 수정_220606
+  // 검색 후 로그아웃 하고 다시 로그인 하면 검색 결과가 그대로 남아있는 문제 발생
+  // 검색 결과 제거 작업에 있어 검색 결과가 있을 때는 allRemoveSticker(), 결과가 없을 때는 그림 제거
+  // 스티커 또는 사진 제거 후 다시 스티커 붙이기
+  allRemoveSticker();
+  noSearchNotice.classList.add(HIDDEN);
+  stickers.forEach(attachSticker);
 
   boardDisplay();
 };
