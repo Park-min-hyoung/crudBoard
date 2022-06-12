8fc5b68ba6bbb5baafc7ffed7592e7e9547b4177
Park-min-hyoung <qkralswgud1324@naver.com>
2022-06-12 14:23:47 +0900

    🔨 Refactor: Add modal.js에 ES6 문법
    Arrow function 사용

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -8,30 +8,24 @@ const makeStickerButton = document.querySelector(
   ".board-form-container button"
 );
 
-// import 사용(board.js에서 가져오기)
-const formInputTitle = document.querySelector(".board-form-container input");
-const formInputTextArea = document.querySelector(
-  ".board-form-container textarea"
-);
-
-function openModal() {
+let openModal = () => {
   // by 민형, board.js의 HIDDEN 사용_220611
   modal.classList.remove(importBoard.HIDDEN);
-}
+};
 
-function closeModal() {
+let closeModal = () => {
   modal.classList.add(importBoard.HIDDEN);
-}
+};
 
 // by 민형, 스티커 생성 폼에 제목과 내용을 작성했을 때만 closeModal 호출_220605
-function isValueCloseModal() {
+let isValueCloseModal = () => {
   if (
     importBoard.stickerTitle.value !== "" &&
     importBoard.stickerContent.value !== ""
   ) {
     closeModal();
   }
-}
+};
 
 modalOpenButton.addEventListener("click", openModal);
 modalOverlay.addEventListener("click", closeModal);
